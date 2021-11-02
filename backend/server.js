84d2e8713bdf5e9bfa65097fb55fd5c37b36ce61
Abhinav Sinha <76272472+abhinav-sinhaa@users.noreply.github.com>
2021-11-02 11:04:33 +0530

    changed env variables

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,21 @@
 const dotenv = require('dotenv');
 const mysql = require('mysql');
-dotenv.config({ path: './config.env' });
 const app = require('./app');
+
+dotenv.config({ path: './config.env' });
 
 /*
   IF ERROR:  Client does not support authentication protocol requested by server; consider 
              upgrading MySQL client, RUN QUERY code 0S<db.init.sql> on workbench 
 */
 const db = mysql.createConnection({
-  host: 'localhost',
-  port: process.env.dbPORT,
-  user: process.env.USER,
-  password: process.env.PASSWORD,
-  database: 'ecom'
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
+
 db.connect(function (err) {
   if (err) {
     console.error('Error connecting ro the Db: ' + err.stack);
@@ -34,6 +36,7 @@ process.on('unhandledRejection', (err) => {
     process.exit(1);
   });
 });
+
 process.on('uncaughtException', (err) => {
   console.log(err.name, err.message);
   console.log('Uncaught exception has occluded, shutting the server down');
