9e211453c555b49007e55c2d4e953afc464b0f97
rhythmshandlya <armaanbgp@gmail.com>
2021-11-03 12:11:36 +0530

    navbar done

--- backend/server.js
+++ backend/server.js
@@ -1,50 +1,23 @@
-const dotenv = require('dotenv');
 const app = require('./app');
 
-dotenv.config({ path: './config.env' });
-
-<<<<<<< HEAD
-app.listen(process.env.PORT, () => {
-=======
-/*
-  IF ERROR:  Client does not support authentication protocol requested by server; consider 
-             upgrading MySQL client, RUN QUERY code 0S<db.init.sql> on workbench 
-*/
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
-});
-
-db.connect(function (err) {
-  if (err) {
-    console.error('Error connecting ro the Db: ' + err.stack);
-    return;
-  }
-  console.log('Db connected. Connected as id ' + db.threadId);
-});
-
 const server = app.listen(process.env.PORT, () => {
->>>>>>> 2ee2f7c5ea19266d1fec46b35039262391516476
   console.log(`Api running on ${process.env.PORT}`);
 });
 
-// process.on('unhandledRejection', (err) => {
-//   console.log(err.name, err.message);
-//   console.log('Unhandled error occluded, shutting the server down');
-//   server.close(() => {
-//     db.end();
-//     process.exit(1);
-//   });
-// });
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('Unhandled error occluded, shutting the server down');
+  server.close(() => {
+    db.end();
+    process.exit(1);
+  });
+});
 
-// process.on('uncaughtException', (err) => {
-//   console.log(err.name, err.message);
-//   console.log('Uncaught exception has occluded, shutting the server down');
-//   server.close(() => {
-//     db.end();
-//     process.exit(1);
-//   });
-// });
+process.on('uncaughtException', (err) => {
+  console.log(err.name, err.message);
+  console.log('Uncaught exception has occluded, shutting the server down');
+  server.close(() => {
+    db.end();
+    process.exit(1);
+  });
+});
--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -16,30 +16,65 @@ const Navbar = () => {
       });
     });
   }, []);
-
+  const hamClick = () => {
+    const ham = document.getElementById("ham");
+    if (!ham.classList.contains("opened")) {
+      document.querySelector(".nav-menu").style.width = "100vw";
+    } else {
+      document.querySelector(".nav-menu").style.width = "0vw";
+    }
+    ham.classList.toggle("opened");
+    ham.setAttribute("aria-expanded", ham.classList.contains("opened"));
+  };
   return (
     <nav>
-      <div className="hamburger">
-        <div className="line"></div>
-        <div className="line"></div>
-        <div className="line"></div>
-      </div>
-      <ul className="nav-links">
-        <li>
-          <span className="nav-brand">Shoe Mania</span>
-        </li>
-        <li>
-          <a href="/home">Home</a>
-          <a href="/category">Category</a>
-          <a href="contact">Contact</a>
-        </li>
-        <li className="to-left">
+      <div className="nav-mb">
+        <button className="hamburger" onClick={hamClick} aria-label="Main Menu">
+          <svg id="ham" width="80" height="50" viewBox="0 0 100 100">
+            <path
+              className="line line1"
+              d="M 20,29.000046 H 80.000231 C 80.000231,29.000046 94.498839,28.817352 94.532987,66.711331 94.543142,77.980673 90.966081,81.670246 85.259173,81.668997 79.552261,81.667751 75.000211,74.999942 75.000211,74.999942 L 25.000021,25.000058"
+            />
+            <path className="line line2" d="M 20,50 H 80" />
+            <path
+              className="line line3"
+              d="M 20,70.999954 H 80.000231 C 80.000231,70.999954 94.498839,71.182648 94.532987,33.288669 94.543142,22.019327 90.966081,18.329754 85.259173,18.331003 79.552261,18.332249 75.000211,25.000058 75.000211,25.000058 L 25.000021,74.999942"
+            />
+          </svg>
+        </button>
+        <span className="nav-brand">Shoe Mania</span>
+        <div className="nav-menu">
+          <a className="menu-link" href="/home">
+            Home
+          </a>
+          <a className="menu-link" href="/category">
+            Category
+          </a>
+          <a className="menu-link" href="/contact">
+            Contact
+          </a>
           <LoginBtn />
-        </li>
-        <li className="">
           <Search />
-        </li>
-      </ul>
+        </div>
+      </div>
+      <div className="nav-cp">
+        <ul className="nav-links">
+          <li>
+            <span className="nav-brand">Shoe Mania</span>
+          </li>
+          <li className="nav-navigation">
+            <a href="/home">Home</a>
+            <a href="/category">Category</a>
+            <a href="/contact">Contact</a>
+          </li>
+          <li className="to-left">
+            <LoginBtn />
+          </li>
+          <li className="">
+            <Search />
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 };
