63d7c3e1a24bbfa5677184343685a392df23cd7a
Abhinav Sinha <76272472+abhinav-sinhaa@users.noreply.github.com>
2021-11-08 20:55:42 +0530

    product record js file

---
+++ backend/Database/product-record.js
@@ -0,0 +1,20 @@
+// Category - Sneakers, Casual shoes, Formal shoes, Flip-flops
+
+[
+  {
+    name: 'Nike Air Force 1 Low Cozi By You',
+    desc: 'Position your feet for the chill in this cosy AF-1, featuring a warm, fuzzy collar liner that feels soft and extends a bit over the top edge for classic cold-weather appeal.Make this hoops-inspired legend your very own, customising details from the materials on the top to the sole underfoot.',
+    brand: 'Nike',
+    category: 'Sneakers',
+    image1:
+      'https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/2e4422d9-80ed-42ce-8842-e44028e27e32/custom-nike-air-force-1-low-cozi-custom-shoe.png',
+    image2:
+      'https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5,q_80/ae10a516-9d70-4de4-9ba8-f8e8b32f7831/custom-nike-air-force-1-low-cozi-custom-shoe.png',
+    image3:
+      'https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5,q_80/33f4eb66-84a5-449d-bd08-26468b68d867/custom-nike-air-force-1-low-cozi-custom-shoe.png',
+    price: 11495,
+    quantity: 4,
+    rating: 4.3,
+    size: '3,4,5,6,7.5,8.5,9'
+  }
+];
