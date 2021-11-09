701ec6dad12c7024760b4b5292372697b510bea9
raghavvij30 <raghavvij30@gmail.com>
2021-11-09 21:36:15 +0530

    New stock of shoes

--- backend/Database/product_record.js
+++ backend/Database/product_record.js
@@ -34,22 +34,7 @@
         rating: 4.7,
         size: '7.5,8.5,9'
       },
-      {
-        name: 'GRAND COURT BASE SHOES',
-        desc: 'A mash-up of celebrated court styles. These 70s-inspired shoes have a smooth leather-like upper. They flash perforated 3-Stripes on one side and stitched contrast 3-Stripes on the other. Ultra-soft cushioning ensures a smooth ride.',
-        brand: 'Adidas',
-        category: 'Sneakers',
-        image1:
-          'https://assets.adidas.com/images/h_840,f_auto,q_auto:sensitive,fl_lossy,c_fill,g_auto/905e7edc0cfc46d7b504aa4300cca484_9366/Grand_Court_Base_Shoes_White_EE7901_01_standard.jpg',
-        image2:
-          'https://assets.adidas.com/images/h_840,f_auto,q_auto:sensitive,fl_lossy,c_fill,g_auto/1d5b54a6398f43fcaedfaa4300ccbbdf_9366/Grand_Court_Base_Shoes_White_EE7901_02_standard_hover.jpg',
-        image3:
-          'https://assets.adidas.com/images/h_840,f_auto,q_auto:sensitive,fl_lossy,c_fill,g_auto/df02bafd06dd49a8a670aa4300ccde15_9366/Grand_Court_Base_Shoes_White_EE7901_05_standard.jpg',
-        price: 4599,
-        quantity: 3,
-        rating: 4.1,
-        size: '5,8.5,9,10'
-      },
+     
       {
         name: 'GRAND COURT BASE SHOES',
         desc: 'A mash-up of celebrated court styles. These 70s-inspired shoes have a smooth leather-like upper. They flash perforated 3-Stripes on one side and stitched contrast 3-Stripes on the other. Ultra-soft cushioning ensures a smooth ride.',
@@ -92,12 +77,232 @@
         image2:
           'https://assets.adidas.com/images/h_840,f_auto,q_auto:sensitive,fl_lossy,c_fill,g_auto/72a8625a8d534cd79f6dadb900aeb2b7_9366/Strret_Icon_M_Black_EY3179_06_standard.jpg',
         image3:
-          'https://assets.adidas.com/images/h_2000,f_auto,q_auto:sensitive,fl_lossy,c_fill,g_auto/708d7b0bdb4141bf848cadb900ae7294_9366/Strret_Icon_M_Black_EY3179_05_standard.jpg,
+          'https://assets.adidas.com/images/h_2000,f_auto,q_auto:sensitive,fl_lossy,c_fill,g_auto/708d7b0bdb4141bf848cadb900ae7294_9366/Strret_Icon_M_Black_EY3179_05_standard.jpg',
         price: 7999,
         quantity: 6,
         rating: 3.5,
         size: '5,8,10'
-      }
+      },
 
-    
+      {
+        name: 'Air Jordan XXXVI First Light PF',
+        desc: 'Since 1985, Jordan Brand has always done things that have never been seen before.Almost 40 years later, Jordan continues to bring new light to the game with international athletes that are redefining the blueprint once again.Beaming with talent, its about the shine within that makes the new faces of the game radiant on the court.Drawing inspiration from ultraviolet rays, the Air Jordan XXXVI First Light celebrates the future of Jordan—the ones who will brighten the way for the next generation of global superstars all across the world.This PF version uses an extra-durable outsole thats designed for outdoor courts.',
+        brand: 'Nike',
+        category: 'Sneakers',
+        image1:
+          'https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5,q_80/fc05405c-cfbe-4147-999f-adda84d4ccdc/air-jordan-xxxvi-first-light-pf-basketball-shoes-fjPfDg.png',
+        image2:
+          'https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5,q_80/b4bc1eff-52fa-4bf6-ab4e-e98b6bf27e0a/air-jordan-xxxvi-first-light-pf-basketball-shoes-fjPfDg.png',
+        image3:
+          'https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/12145a62-d3d8-469e-a35a-05320c043b6a/air-jordan-xxxvi-first-light-pf-basketball-shoes-fjPfDg.png',
+        price: 14599,
+        quantity: 11,
+        rating: 4.6,
+        size: '9,10,11'
+      },
+
+
+      {
+        name: 'Nike Air Max Camden',
+        desc: 'Inspired by an icon, the Nike Air Max Camden Slide uses soft foam, deep flex grooves and a cushioned upper strap to give you 360 degrees of comfort.',
+        brand: 'Nike',
+        category: 'Flip-flops',
+        image1:
+          'https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,b_rgb:f5f5f5/55cad507-9aa1-4e94-9269-057e32d975db/air-max-camden-slide-qw8C8G.png',
+        image2:
+          'https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,b_rgb:f5f5f5,q_80/172afc34-0310-479e-8e5d-5695459f9268/air-max-camden-slide-qw8C8G.png',
+        image3:
+          'https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,b_rgb:f5f5f5,q_80/3755c940-29f7-4402-b7b8-70d9ad427f2b/air-max-camden-slide-qw8C8G.png',
+        price: 4999,
+        quantity: 16,
+        rating: 4.8,
+        size: '5,8,10'
+      },
+      {
+        name: 'Nike Air Force 107 Premium',
+        desc: 'The radiance lives on in the Nike Air Force 107 Premium, the b-ball icon that puts a fresh spin on what you know best. It has stitched overlays, bold colours and the perfect amount of hoops style to make heads turn.',
+        brand: 'Nike',
+        category: 'Sneakers',
+        image1:
+          'https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/964f652f-15d9-40eb-ba7c-aa6e264eea24/air-force-1-07-shoes-McM853.png',
+        image2:
+          'https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5,q_80/ce7392f2-c7fc-4fac-8237-504493e8584a/air-force-1-07-shoes-McM853.png',
+        image3:
+          'https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5,q_80/3cef5ada-f293-4bce-a3cb-f30e1949b7dd/air-force-1-07-shoes-McM853.png',
+        price: 11990,
+        quantity: 15,
+        rating: 4,
+        size: '6,7,9,11'
+      },
+     
+      {
+        name: 'Nike Victori One',
+        desc: 'You have played hard. You have sweated. You have given your all. Now there is just one last thing—you have to hit the showers. Dont worry, we have got your back with the Nike Victori One. Its one-piece foam design feels soft and comfortable while traction patterns on the sole help with wet surfaces. They dry quickly too, so no one will know you have just stepped out of the showers as you slide up to the party.',
+        brand: 'Nike',
+        category: 'Flip-flops',
+        image1:
+          'https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/1abc3630-7318-4ca7-83fc-9298dd9f5be1/victori-one-shower-slide-KPPNdb.png',
+        image2:
+          'https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5,q_80/98619529-e27a-4265-a372-4dc8b3592967/victori-one-shower-slide-KPPNdb.png',
+        image3:
+          'https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5,q_80/48d6d3a9-6917-411b-b7d4-c2e7b05aaf61/victori-one-shower-slide-KPPNdb.png',
+        price: 1990,
+        quantity: 21,
+        rating: 3.6,
+        size: '6,7,8,9'
+      },
+
+     
+      {
+        name: 'Nike Offline 2.0',
+        desc: 'Refresh and relax in the Nike Offline 2.0 Mule.Its micro-bead insole creates a subtle texture that massages your feet while the padded, bootie-like upper feels soft and airy.Rounded laces bring it into a new realm and let you personalise your fit, while the cork and rubber heel piece contrasts against the technical upper for a modern look.We think it is OK to do nothing—just do it comfortably.',
+        brand: 'Nike',
+        category: 'Flip-flops',
+        image1:
+          'https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/c5e9c7a8-f02c-41aa-8923-f4a13fa39708/offline-2-mule-ZH6FSs.png',
+        image2:
+          'https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5,q_80/ecd4d8af-0b10-450f-a502-8587ed20f6f9/offline-2-mule-ZH6FSs.png',
+        image3:
+          'https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5,q_80/ccb3d2f5-b68e-4487-a552-b4bdaa48eeb2/offline-2-mule-ZH6FSs.png',
+        price: 15990,
+        quantity: 10,
+        rating: 4.4,
+        size: '9,,10,11,13'
+      },
+
+      {
+        name: 'Nike Court Vision Low',
+        desc: 'So you re in love with the classic look of 80s basketball, but you ve got a thing for the fast-paced culture of todays game. Meet the new Nike Court Vision Low. Its crisp upper and stitched overlays are inspired by the hook shot of old-school basketball while its super-plush, low-cut collar adds a sleek look thats comfortable day in and day out.',
+        brand: 'Nike',
+        category: 'Sneakers',
+        image1:
+          'https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/ca941236-2a66-47d6-8949-318225a4e6a7/court-vision-low-shoe-bv8p57.png',
+        image2:
+          'https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5,q_80/765b2ba5-5177-4e28-8b5f-3c7fe9cb1133/court-vision-low-shoe-bv8p57.png',
+        image3:
+          'https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5,q_80/91d063b8-f12b-4006-8f84-e16a27f155a4/court-vision-low-shoe-bv8p57.png',
+        price: 4955,
+        quantity: 9,
+        rating: 4.1,
+        size: '6,7,10,11'
+      },
+
+      {
+        name: 'Nike Dunk Low SE',
+        desc: 'Created for the hardwood but taken to the streets, the 80s b-ball icon returns with animal prints and metallic hits that let your primal side shine through.With its classic hoops design, the Nike Dunk Low SE channels vintage style, rocking a padded, low-cut collar so you can take your game anywhere—in comfort.',
+        brand: 'Nike',
+        category: 'Sneakers',
+        image1:
+          'https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/d16e7b66-1c16-4b7d-9d5f-6495588c7a31/dunk-low-se-shoes-zmKqzw.png',
+        image2:
+          'https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5,q_80/ea588439-8d2a-4827-bc37-ef1a003debe8/dunk-low-se-shoes-zmKqzw.png',
+        image3:
+          'https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5,q_80/902c85d2-f5bc-4b34-8532-ce16f709c88d/dunk-low-se-shoes-zmKqzw.png',
+        price: 9990,
+        quantity: 10,
+        rating: 4.9,
+        size: '3,5,6,7,8'
+      },
+
+      {
+        name: 'MENS REEBOK CLASSICS CASUAL ROYAL COMPLETE 2 PX SHOES',
+        desc: 'Classic. Sophisticated. Eternal. This mens footwear will satisfy all your retro style needs. Designed with a strong vulcanized sole, this pair will keep your feet fresh and fashionable. In addition, its mesh upper and synthetic fabric combines modernity with flexibility.',
+        brand: 'Reebok',
+        category: 'Casual',
+        image1:
+          'https://content.shop4reebok.com/static/Product-CM9826/Men_CASUAL_SHOES_LOW_CM9826_1.jpg',
+        image2:
+          'https://content.shop4reebok.com/static/Product-CM9826/Men_CASUAL_SHOES_LOW_CM9826_4.jpg',
+        image3:
+          'https://content.shop4reebok.com/static/Product-CM9826/Men_CASUAL_SHOES_LOW_CM9826_3.jpg',
+        price: 4999,
+        quantity: 11,
+        rating: 3.7,
+        size: '6,7,8,9,11'
+      },
+
+      {
+        name: 'MENS REEBOK SWIM FULGERE SLIDES',
+        desc: 'These mens slides offer an easygoing look for the beach or the swimming pool. The soft moulded EVA upper provides comfort and support. Made of water-friendly material, they are built for easy on and off.',
+        brand: 'Reebok',
+        category: 'Flip-flops',
+        image1:
+          'https://content.shop4reebok.com/static/Product-FZ0947/reebok_FZ0947_1.jpg',
+        image2:
+          'https://content.shop4reebok.com/static/Product-FZ0947/reebok_FZ0947_3.jpg',
+        image3:
+          'https://content.shop4reebok.com/static/Product-FZ0947/reebok_FZ0947_5.jpg',
+        price: 1990,
+        quantity: 7,
+        rating: 4.4,
+        size: '4,5.5,6,7,9'
+      },
+
+      {
+        name: 'UNISEX REEBOK CLASSIC SLIDES',
+        desc: 'Your classic style comes effortlessly, and we crafted these shoes just for you. From the court to weekend outings, these slides slip on in a flash for athletic-inspired looks and comfort.',
+        brand: 'Reebok',
+        category: 'Flip-flops',
+        image1:
+          'https://content.shop4reebok.com/static/Product-FW5751/reebok_FW5751_1.jpg',
+        image2:
+          'https://content.shop4reebok.com/static/Product-FW5751/reebok_FW5751_3.jpg',
+        image3:
+          'https://content.shop4reebok.com/static/Product-FW5751/reebok_FW5751_5.jpg',
+        price: 4999,
+        quantity: 10,
+        rating: 2.9,
+        size: '6,10,11'
+      },
+
+      {
+        name: 'MENS REEBOK RUNNING ENERGY RUNNER LP SHOES',
+        desc: 'For your constant mover, these mens shoes are a lifesaver. The durable outsole helps give great traction to keep em on their feet. Plus an iconic logo along the sidewall keeps the athletic style fresh.',
+        brand: 'Reebok',
+        category: 'Casual',
+        image1:
+          'https://content.shop4reebok.com/static/Product-FW1936/MEN_RUNNING_SHOES_FW1936_1.jpg',
+        image2:
+          'https://content.shop4reebok.com/static/Product-FW1936/MEN_RUNNING_SHOES_FW1936_3.jpg',
+        image3:
+          'https://content.shop4reebok.com/static/Product-FW1936/MEN_RUNNING_SHOES_FW1936_4.jpg',
+        price: 7990,
+        quantity: 8,
+        rating: 3.3,
+        size: '6,9,11,13'
+      },
+
+      {
+        name: 'MENS REEBOK RUNNING BRIDGEPORT SHOES',
+        desc: 'Run your daily miles in absolute comfort with the Reebok Bridgeport Shoes. The two-toned breathable knit upper keeps your feet cool with every energised step and the EVA midsole and outsole provides cushioned comfort.',
+        brand: 'Reebok',
+        category: 'Casual',
+        image1:
+          'https://content.shop4reebok.com/static/Product-EX4164/reebok_EX4164_1.jpg',
+        image2:
+          'https://content.shop4reebok.com/static/Product-EX4164/reebok_EX4164_2.jpg',
+        image3:
+          'https://content.shop4reebok.com/static/Product-EX4164/reebok_EX4164_4.jpg.',
+        price: 2990,
+        quantity: 20,
+        rating: 4.3,
+        size: '4,6,7,9'
+      },
+
+      {
+        name: 'MENS REEBOK RUNNING OFF ROADER SHOES',
+        desc: 'Run your daily miles in absolute comfort with the Reebok Off Roader shoes. The breathable mesh upper keeps your feet cool with every energised step while the PU overlays enhance durability.The EVA midsole and outsole provides cushioned comfort',
+        brand: 'Reebok',
+        category:'Casual',
+        image1:
+          'https://content.shop4reebok.com/static/Product-EX4258/reebok_EX4258_1.jpg',
+        image2:
+          'https://content.shop4reebok.com/static/Product-EX4258/reebok_EX4258_3.jpg',
+        image3:
+          'https://content.shop4reebok.com/static/Product-EX4258/reebok_EX4258_4.jpg.',
+        price: 3999,
+        quantity: 18,
+        rating: 2.8,
+        size: '5,6,7,8,9,11'
+      }
   ];
