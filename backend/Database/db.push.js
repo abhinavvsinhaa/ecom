457ab8d0038a8f5edd39464e39032ee5f4798183
rhythmshandlya <armaanbgp@gmail.com>
2021-11-16 21:13:59 +0530

    yeh

--- backend/Database/db.push.js
+++ backend/Database/db.push.js
@@ -1,8 +1,22 @@
-const db = require('../db-initialize');
 const mysql = require('mysql');
-
 const fs = require('fs');
-fs.readFile('./product.json', 'utf8', (err, jsonString) => {
+const path = require('path');
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  port: 6969,
+  user: 'root',
+  password: 'balleballe',
+  database: 'ecom'
+});
+
+db.connect(function (err) {
+  if (err) {
+    console.error('Error connecting to the Db: ' + err.stack);
+    return;
+  }
+});
+fs.readFile(path.join(__dirname, 'product.json'), 'utf8', (err, jsonString) => {
   if (err) {
     console.log('File read failed:', err);
     return;
@@ -10,22 +24,32 @@ fs.readFile('./product.json', 'utf8', (err, jsonString) => {
   const arr = JSON.parse(jsonString);
   arr.forEach((obj) => {
     db.query(
-      `INSERT INTO ecom.product (
-            name,
-            short_desc,
-            brand,
-            category,
-            image_link1,
-            image_link2,
-            image_link3,
-            price,
-            quantity,
-            rating,
-            size
-          ) VALUES(${obj.name},${obj.short_desc},${obj.brand},${obj.category},${obj.image_link1},${obj.image_link2},${obj.image_link3},${obj.price},${obj.quantity},${obj.rating},${obj.size});`,
+      'INSERT INTO ecom.product (name,short_desc,brand,category,image_link1,image_link2,image_link3,price,quantity,rating,size) VALUES("' +
+        obj.name +
+        '","' +
+        obj.desc +
+        '","' +
+        obj.brand +
+        '","' +
+        obj.category +
+        '","' +
+        obj.image1 +
+        '","' +
+        obj.image2 +
+        '","' +
+        obj.image3 +
+        '",' +
+        obj.price +
+        ',' +
+        obj.quantity +
+        ',' +
+        obj.rating +
+        ',"' +
+        obj.size +
+        '");',
       (err) => {
         if (err) throw err;
-        log(`Product added successfully with id: ${db_res.insertId}`);
+        console.log(`Product added successfully`);
       }
     );
   });
