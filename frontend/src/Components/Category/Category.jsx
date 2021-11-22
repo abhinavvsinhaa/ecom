58d8b21686ce23fcdd5056e97e7aacda4ddecb75
rhythmshandlya <armaanbgp@gmail.com>
2021-11-22 13:52:20 +0530

    xx

--- frontend/src/Components/Category/Category.jsx
+++ frontend/src/Components/Category/Category.jsx
@@ -5,31 +5,36 @@ import api from "./../../Util/api";
 import "./category.css";
 const options = { withCredentials: true };
 const Price = () => {
+  React.useEffect(() => {
+    document.querySelector(".cat-form").addEventListener("submit", (e) => {
+      e.preventDefault();
+    });
+  }, []);
   return (
-    <form className="price-range">
+    <form className="cat-form">
       <div className="filter-div">
         <p className="filter-label">Price range</p>
-        <div>
+        <div className="cat-flex">
           <input type="radio" id="html" name="fav_language" value="HTML" />
           <label for="html">₹0-₹2000</label>
         </div>
-        <div>
-          <input type="radio" id="html" name="fav_language" value="HTML" /> 
+        <div className="cat-flex">
+          <input type="radio" id="html" name="fav_language" value="HTML" />
           <label for="html">₹2000-₹5000</label>
         </div>
-        <div>
-          <input type="radio" id="html" name="fav_language" value="HTML" /> 
+        <div className="cat-flex">
+          <input type="radio" id="html" name="fav_language" value="HTML" />
           <label for="html">₹5000-₹10000</label>
         </div>
-        <div>
-          <input type="radio" id="html" name="fav_language" value="HTML" /> 
+        <div className="cat-flex">
+          <input type="radio" id="html" name="fav_language" value="HTML" />
           <label for="html">₹10000-MAX</label>
         </div>
       </div>
       <div className="filter-div">
         <p className="filter-label">Sort By</p>
         <div>
-          <select name="sort-by" id="cars">
+          <select className="cat-select" name="sort-by" id="cars">
             <option value="">Price: Low to High</option>
             <option value="">Price: High to Low</option>
             <option value="">Popular</option>
@@ -37,39 +42,43 @@ const Price = () => {
           </select>
         </div>
       </div>
-      <div className="filter-div">
+      <div className="cat-flex">
         <input type="checkbox" id=""></input>
         <label for="">Exclude Out Of Stock</label>
       </div>
       <div className="filter-div">
         <p className="filter-label">Rating</p>
-        <div>
+        <div className="cat-flex">
           <input type="radio" id="html" name="fav_language" value="HTML" />
           <label for="html">Above 4</label>
         </div>
-        <div>
-          <input type="radio" id="html" name="fav_language" value="HTML" /> 
+        <div className="cat-flex">
+          <input type="radio" id="html" name="fav_language" value="HTML" />
           <label for="html">Above 3</label>
         </div>
-        <div>
-          <input type="radio" id="html" name="fav_language" value="HTML" /> 
+        <div className="cat-flex">
+          <input type="radio" id="html" name="fav_language" value="HTML" />
           <label for="html">Above 2</label>
         </div>
-        <div>
-          <input type="radio" id="html" name="fav_language" value="HTML" /> 
+        <div className="cat-flex">
+          <input type="radio" id="html" name="fav_language" value="HTML" />
           <label for="html">Above 1</label>
         </div>
       </div>
       <div className="filter-div">
         <p className="filter-label">Select Brands</p>
-        <input type="checkbox" id=""></input>
-        <label for="">Nike</label>
-        <br />
-        <input type="checkbox" id=""></input>
-        <label for="">Adidas</label>
-        <br />
-        <input type="checkbox" id=""></input>
-        <label for="">Rebok</label>
+        <div className="cat-flex">
+          <input type="checkbox" id=""></input>
+          <label for="">Nike</label>
+        </div>
+        <div className="cat-flex">
+          <input type="checkbox" id=""></input>
+          <label for="">Adidas</label>
+        </div>
+        <div className="cat-flex">
+          <input type="checkbox" id=""></input>
+          <label for="">Rebok</label>
+        </div>
       </div>
       <input type="submit" className="submit-filter" value="Commit Change" />
     </form>
--- frontend/src/Components/Login/Login.jsx
+++ frontend/src/Components/Login/Login.jsx
@@ -1,88 +1,101 @@
-import React, {useState} from 'react';
-import './Login.css'
-import Logo from '../../Assets/logo.jpeg';
-import Navbar from '../Navbar/Navbar';
-import bcrypt from 'bcryptjs'
+import React, { useState } from "react";
+import "./Login.css";
+import Logo from "../../Assets/logo.jpeg";
+import Navbar from "../Navbar/Navbar";
+import bcrypt from "bcryptjs";
 
 const Login = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const getUser = (e) => {
-        e.preventDefault();
-        fetch("http://localhost:8080/api/v1/user/login", {
-            method: "POST",
-            headers: {
-                "Content-Type":"application/json"
-            },
-            body: JSON.stringify({email, password})
-        })
-        .then(res => res.json())
-        .then(response => {
-            if (response.password) {
-                bcrypt.compare(password, response.password, (err, res) => {
-                    if (err) console.log(err)
-                    if (res === true) {
-                        console.log("User logged in")
-                    } else {
-                        console.log("Incorrect email or password")
-                    }
-                })
+  const getUser = (e) => {
+    e.preventDefault();
+    fetch("http://localhost:8080/api/v1/user/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    })
+      .then((res) => res.json())
+      .then((response) => {
+        if (response.password) {
+          bcrypt.compare(password, response.password, (err, res) => {
+            if (err) console.log(err);
+            if (res === true) {
+              console.log("User logged in");
+            } else {
+              console.log("Incorrect email or password");
             }
-        })
-        .catch(console.log)
-    }
+          });
+        }
+      })
+      .catch(console.log);
+  };
 
-    return(
+  return (
     <div className="container-fluid login">
-        <div className="row navbar-row">
-            <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-                <Navbar/>
-            </div>
+      <div className="row navbar-row">
+        <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+          <Navbar />
         </div>
-        <div className="login-space"></div>
-        <div className="row justify-content-center login-form-row">
-            <div className="col-xl-8 col-lg-8 col-md-8 col-sm-10 col-12 form-container">
-                <div className="login-form">
-                    <img src={Logo} alt="ShoeMania Logo" className="login-shoemania-logo"/>
-                    <div className="form-content">
-                        <p className="login-heading">Login</p>
-                        <form onSubmit={getUser}>
-                        <input 
-                        type="email" 
-                        name="email" 
-                        id="email" 
-                        placeholder="Email" 
-                        required
-                        onChange={e => setEmail(e.target.value)}
-                        />
-                        <br /><br />
-                        <input 
-                        type="password" 
-                        name="password" 
-                        id="password"
-                        placeholder="Password"
-                        required
-                        onChange={e => setPassword(e.target.value)}
-                        />
-                        <br /><br />
-                        <p className="terms">By continuing I agree to the  
-                        <a href="/" className="terms-link">
-                        &nbsp;Terms of use
-                        </a>
-                        &nbsp;&&nbsp;
-                        <a href="/" className="terms-link">
-                        Privacy policy.
-                        </a> 
-                        </p>
-                        <input type="submit" value="CONTINUE" className="btn btn-primary"/>
-                        </form>
-                    </div>
-                </div>
+      </div>
+      <div className="login-space"></div>
+      <div className="row justify-content-center login-form-row">
+        <div className="col-xl-8 col-lg-8 col-md-8 col-sm-10 col-12 form-container">
+          <div className="login-form">
+            <img
+              src={Logo}
+              alt="ShoeMania Logo"
+              className="login-shoemania-logo"
+            />
+            <div className="form-content">
+              <p className="login-heading">Login</p>
+              <form onSubmit={getUser}>
+                <input
+                  type="email"
+                  name="email"
+                  className="login-input"
+                  id="email"
+                  placeholder="Email"
+                  required
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <br />
+                <br />
+                <input
+                  type="password"
+                  name="password"
+                  className="login-input"
+                  id="password"
+                  placeholder="Password"
+                  required
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <br />
+                <br />
+                <p className="terms">
+                  By continuing I agree to the
+                  <a href="/" className="terms-link">
+                    &nbsp;Terms of use
+                  </a>
+                  &nbsp;&&nbsp;
+                  <a href="/" className="terms-link">
+                    Privacy policy.
+                  </a>
+                </p>
+                <input
+                  type="submit"
+                  value="CONTINUE"
+                  className="btn btn-primary"
+                />
+              </form>
             </div>
+          </div>
         </div>
+      </div>
     </div>
-    );
-}
+  );
+};
 
 export default Login;
